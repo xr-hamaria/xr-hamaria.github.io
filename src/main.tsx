9b378ffbf4f1a8e9be94ae5e00f48131caa0dec1
Naborisk <naborisk@gmail.com>
2023-03-02 20:21:21 +0900

    add Google Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import ReactGA from 'react-ga4'
+
+ReactGA.initialize('G-7QR39XZN5F')
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
